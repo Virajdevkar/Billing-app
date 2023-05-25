e766ac1cc6374d327dbfbeb67a949daf6017da61
Virajdevkar <91430666+Virajdevkar@users.noreply.github.com>
2023-05-25 15:34:31 +0530

    Update SignupPage.js

--- SignupPage.js
+++ SignupPage.js
@@ -7,6 +7,8 @@ const SignupPage = () => {
   const [mobileNumber, setMobileNumber] = useState('');
   const [shopName, setShopName] = useState('');
   const [shopAddress, setShopAddress] = useState('');
+  const [shopLocation, setshopLocation] = useState('');
+  const [Pincode, setPincode] = useState('');
 
   const handleSignup = (e) => {
     e.preventDefault();
@@ -19,6 +21,8 @@ const SignupPage = () => {
     console.log('Mobile Number:', mobileNumber);
     console.log('Shop Name:', shopName);
     console.log('Shop Address:', shopAddress);
+    console.log('shop Location:', shopLocation);
+    console.log('Pincode:', Pincode);
     // Reset form fields
     setUsername('');
     setEmail('');
@@ -26,47 +30,62 @@ const SignupPage = () => {
     setMobileNumber('');
     setShopName('');
     setShopAddress('');
+    setshopLocation('');
+    setPincode('');
   };
 
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-      <div style={{ border: '1px solid #ccc', borderRadius: '5px', padding: '20px', width: '900px' }}>
-        <h2 style={{ textAlign: 'center', marginBottom: '20px' }}>Signup Page</h2>
+    <>
+      
+      <h1 style={{ textAlign: 'center', marginBottom: '20px',font:'poppins',fontSize:'49px' }}>SIGN-UP</h1>
+
+    
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '60vh' }}>
+      <div style={{ border: '1px solid #ccc', borderRadius: '55px', padding: '20px', width: '70%' }}>
+       {/*<h2 style={{ textAlign: 'center', marginBottom: '20px' }}>Signup Page</h2>*/}
         <form onSubmit={handleSignup}>
           <div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr', columnGap: '20px' }}>
             <div>
-              <label htmlFor="username">Username:</label>
-              <input type="text" id="username" value={username} onChange={(e) => setUsername(e.target.value)} style={{ width: '90%', padding: '10px', border: '1px solid #ccc', borderRadius: '5px' }} />
+              <label htmlFor="username">Vendor Name:</label>
+              <input type="text" id="username" value={username} onChange={(e) => setUsername(e.target.value)} style={{ width: '90%', padding: '10px', border: '1px solid #ccc', borderRadius: '15px' }} />
             </div>
             <div>
               <label htmlFor="email">Email:</label>
-              <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} style={{ width: '90%', padding: '10px', border: '1px solid #ccc', borderRadius: '5px' }} />
+              <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} style={{ width: '90%', padding: '10px', border: '1px solid #ccc', borderRadius: '15px' }} />
             </div>
             <div>
               <label htmlFor="password">Password:</label>
-              <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} style={{ width: '90%', padding: '10px', border: '1px solid #ccc', borderRadius: '5px' }} />
+              <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} style={{ width: '90%', padding: '10px', border: '1px solid #ccc', borderRadius: '15px' }} />
             </div>
             <div>
               <label htmlFor="mobileNumber">Mobile Number:</label>
-              <input type="tel" id="mobileNumber" value={mobileNumber} onChange={(e) => setMobileNumber(e.target.value)} style={{ width: '90%', padding: '10px', border: '1px solid #ccc', borderRadius: '5px' }} />
+              <input type="tel" id="mobileNumber" value={mobileNumber} onChange={(e) => setMobileNumber(e.target.value)} style={{ width: '90%', padding: '10px', border: '1px solid #ccc', borderRadius: '15px' }} />
             </div>
             <div>
               <label htmlFor="shopName">Shop Name:</label>
-              <input type="text" id="shopName" value={shopName} onChange={(e) => setShopName(e.target.value)} style={{ width: '90%', padding: '10px', border: '1px solid #ccc', borderRadius: '5px' }} />
+              <input type="text" id="shopName" value={shopName} onChange={(e) => setShopName(e.target.value)} style={{ width: '90%', padding: '10px', border: '1px solid #ccc', borderRadius: '15px' }} />
             </div>
             <div>
               <label htmlFor="shopAddress">Shop Address:</label>
-              <input type="text" id="shopAddress" value={shopAddress} onChange={(e) => setShopAddress(e.target.value)} style={{ width: '90%', padding: '10px', border: '1px solid #ccc', borderRadius: '5px' }} />
+              <input type="text" id="shopAddress" value={shopAddress} onChange={(e) => setShopAddress(e.target.value)} style={{ width: '90%', padding: '10px', border: '1px solid #ccc', borderRadius: '15px' }} />
+            </div>
+            <div>
+              <label htmlFor="shopLocation">Location:</label>
+              <input type="text" id="shopLocation" value={shopLocation} onChange={(e) => setshopLocation(e.target.value)} style={{ width: '90%', padding: '10px', border: '1px solid #ccc', borderRadius: '15px' }} />
+            </div>
+            <div>
+              <label htmlFor="Pincode">Pincode:</label>
+              <input type="text" id="Pincode" value={Pincode} onChange={(e) => setPincode(e.target.value)} style={{ width: '90%', padding: '10px', border: '1px solid #ccc', borderRadius: '15px' }} />
             </div>
           </div>
-          <button type="submit" style={{ width: '30%',alignItems:'center' ,marginLeft: '280px',marginTop:'10px' ,padding: '15px', background: 'orange', color: 'white', border: 'none', borderRadius: '5px', fontSize: '16px', fontWeight: 'bold' }}>Signup</button>
+          <button type="submit" style={{ width: '30%',alignItems:'center' ,marginLeft: '345px',marginTop:'10px' ,padding: '15px', background: '#FF7855', color: 'white', border: 'none', borderRadius: '25px', fontSize: '16px', fontWeight: 'bold' }}>Sign-up</button>
         </form>
       </div>
     </div>
+    </>
   );
 };
 
 export default SignupPage;
-
 
 
